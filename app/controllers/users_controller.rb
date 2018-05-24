24903089ade7e463e4ab91ce29c2d57793e44f05
GitSP917 <GisSP9675@gmail.com>
2018-05-24 08:19:30 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    
+    @responses = @user.responses.order('created_at DESC').page(params[:page])
+    
   end
 
   def new
